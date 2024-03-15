b5353fd5ecbd83a954cc088b2c6f67afe87169da
GADO89 <gadomahmoud377@gmail.com>
2024-03-15 08:41:35 +0200

    first commit

---
+++ src/main/java/io/getarrays/pagination/service/UserService.java
@@ -0,0 +1,31 @@
+package io.getarrays.pagination.service;
+
+
+import io.getarrays.pagination.domain.User;
+import org.springframework.data.domain.Page;
+
+public interface UserService {
+
+  Page<User> getUsers(String name, int page, int size);
+}
+
+
+
+/*
+package io.getarrays.pagination.service;
+
+import io.getarrays.pagination.domain.User;
+import org.springframework.data.domain.Page;
+
+*/
+/**
+ * @author Get Arrays (https://www.getarrays.io/)
+ * @version 1.0
+ * @since 6/26/2022
+ *//*
+
+
+public interface UserService {
+    Page<User> getUsers(String name, int page, int size);
+}
+*/
